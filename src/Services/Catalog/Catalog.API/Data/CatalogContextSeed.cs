909be647f5ddbfde60772670f494bd2fde3ddc1f
Pavel Quevedo <pavel.quevedo@rivetlogic.com>
2022-09-18 19:37:44 -0600

    Adding ProductRepository and CatalogController.

--- src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -5,13 +5,13 @@ namespace Catalog.API.Data
 {
     public class CatalogContextSeed
     {
-        public static async Task SeedData(IMongoCollection<Product> productCollection)
+        public static void SeedData(IMongoCollection<Product> productCollection)
         {
-            bool existProduct = await productCollection.Find(p => true).AnyAsync();
+            bool existProduct = productCollection.Find(p => true).Any();
 
             if (!existProduct)
             {
-                await productCollection.InsertManyAsync(GetPreconfiguredProducts());
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
             }
         }
 
