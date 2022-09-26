10432be6c0edb7238c1d8324b1848891ea78c433
Pavel Quevedo <pavel.quevedo@rivetlogic.com>
2022-09-26 17:51:58 -0600

    Injecting application and infrastructure services to Ordering.API.

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,11 @@
+using Ordering.Application.Extensions;
+using Ordering.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
