978b023bfd64bf549d7d7a5436fdb1bd97840e43
Pavel Quevedo <pavel.quevedo@rivetlogic.com>
2022-09-30 12:37:06 -0600

    Configuring Ocelot cache.

--- src/ApiGateways/Gateway.API/Program.cs
+++ src/ApiGateways/Gateway.API/Program.cs
@@ -1,9 +1,11 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot()
+                .AddCacheManager(settings => settings.WithDictionaryHandle());
 
 
 builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
