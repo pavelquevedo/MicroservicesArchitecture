4411d762f2ec807e2722cb754c76b0afe0f387e7
Pavel Quevedo <pavel.quevedo@rivetlogic.com>
2022-09-30 12:32:01 -0600

    Adding Ocelot gateway configuration.

--- src/ApiGateways/Gateway.API/Program.cs
+++ src/ApiGateways/Gateway.API/Program.cs
@@ -1,7 +1,26 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot();
+
+
+builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
+{
+    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+
+}).ConfigureAppConfiguration((hostingContext, configBuilder) =>
+{
+    configBuilder.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+await app.UseOcelot();
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
